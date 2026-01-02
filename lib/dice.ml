e6487d36aea0560ff96c61898cda626bfb5d7bc9
saiqi <julien.bernard.iphone@gmail.com>
2026-01-02 10:32:04 +0100

    feat: implement dice logic

---
+++ lib/dice.ml
@@ -0,0 +1,5 @@
+let max = 6
+
+let roll n =
+  List.init (Dice_count.to_int n) (fun _ -> 1 + Random.int max)
+  |> List.fold_left ( + ) 0
---
+++ lib/dice.mli
@@ -0,0 +1,2 @@
+val max : int
+val roll : Dice_count.t -> int
