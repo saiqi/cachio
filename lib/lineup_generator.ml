a04760cfe6fe307a45fa4754105d9c7fba9f6df3
saiqi <julien.bernard.iphone@gmail.com>
2026-01-26 01:09:29 +0100

    revert: :rewind: implement cache of boards
    This reverts commit 3938ee96c4dbddd7d11803d1f613033dcd1ca5f2.

--- lib/lineup_generator.ml
+++ lib/lineup_generator.ml
@@ -1,19 +1,3 @@
-type roster_key = Player_id.t list
-
-let roster_key_of_roster roster =
-  Roster.to_list roster |> List.map Player.id |> List.sort Player_id.compare
-
-module RosterKey = struct
-  type t = roster_key
-
-  let equal = ( = )
-  let hash = Hashtbl.hash
-end
-
-module Cache = Hashtbl.Make (RosterKey)
-
-let cache : Board.t list Cache.t = Cache.create 128
-
 let rec combinations k = function
   | [] -> []
   | x :: xs ->
@@ -24,7 +8,7 @@ let rec combinations k = function
 let all_coords =
   List.concat_map (fun r -> List.map (fun c -> (r, c)) Column.all) Row.all
 
-let compute_all roster =
+let all roster =
   let players = Roster.to_list roster in
   let player_sets = combinations Rules.players_on_board players in
   let coords = all_coords in
@@ -43,12 +27,3 @@ let compute_all roster =
       player_sets
   in
   List.filter Board.is_valid candidates
-
-let all roster =
-  let key = roster_key_of_roster roster in
-  match Cache.find_opt cache key with
-  | Some boards -> boards
-  | None ->
-      let boards = compute_all roster in
-      Cache.add cache key boards;
-      boards
