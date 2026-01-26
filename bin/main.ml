c806c4e4d5fb10d874711fd8eea92d02fd279bba
saiqi <julien.bernard.iphone@gmail.com>
2026-01-26 22:41:50 +0100

    feat: :sparkles: add number of sim parameter

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,22 @@
 open Cachio
 
+let num_simulations () =
+  let usage () =
+    prerr_endline "Usage: cachio [num_simulations]";
+    exit 1
+  in
+  match Sys.argv with
+  | [| _ |] -> 1
+  | [| _; n |] -> (
+      match int_of_string_opt n with Some n when n > 0 -> n | _ -> usage ())
+  | _ -> usage ()
+
 let run () =
+  let n = num_simulations () in
   let module R = Rng.Std in
   let rng = R.create () in
   let sim = Simulation.create (module R) rng Scenario.deterministic_ais in
-  let runs = Simulation.run_n 10_000 (module R) rng sim in
+  let runs = Simulation.run_n n (module R) rng sim in
   let audits = runs |> List.map snd |> List.flatten in
   let stats = Stats.of_audits audits in
   let report = Report.of_stats stats in
